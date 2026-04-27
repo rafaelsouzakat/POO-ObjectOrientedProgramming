086f24a8d137aa37e5ff81e9717b0a0797423648
Rafael Souza K <rafahira55@gmail.com>
2026-04-27 19:47:08 +0000

    Mudei o nome da classe ConversorDeTextoTest.java e criei um arquivo txt da saída que tive com os testes.

--- LAB/Lab_09_TestesUnitarios/ConversorDeTextoTestCompleto.java
+++ LAB/Lab_09_TestesUnitarios/ConversorDeTextoTestCompleto.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ConversorDeTextoTest {
+public class ConversorDeTextoTestCompleto {
 
     private ConversorDeTexto conversor;
 
